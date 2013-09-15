238bc33d3db03c389540ee0de279e4d928574489
Holly Grant <holly.u.g@gmail.com>
2013-09-15 15:38:34 +0100

    Totally hacked away at it.. somewhat unecessary! Actually introduced some terrible code practice.. whatever~ tracked down gmail bug further, still to fix.

--- home.js
+++ home.js
@@ -1,12 +1,13 @@
 (function() {
 	var addBookmark, addBookmarks, gmailRe, setGmailCount, store, ul, updateGmailCount;
-
+	
 	ul = document.getElementById("bookmarks");
 
 	store = window.localStorage;
 
 	gmailRe = new RegExp(/https:\/\/mail\.google\.com\/?(a\/.+\/)?/);
 
+	//BUG IS IN HERE ////
 	setGmailCount = function(id, count) {
 		var countTemplate, klass, link, span;
 		klass = count === "0" ? "zero" : "count";
@@ -24,7 +25,8 @@
 		var lastCount, xhr;
 		lastCount = store.getItem(id + "_count");
 		if (lastCount !== null) {
-			setGmailCount(id, lastCount);
+			//setGmailCount(id, lastCount);
+			console.log("HIT");
 		}
 		if ((new Date()).getTime() - store.getItem(id + "_time") > 60000) {
 			xhr = new XMLHttpRequest();
@@ -43,46 +45,80 @@
 			return xhr.send();
 		}
 	};
-
-	addBookmark = function(id, title, url, indent) {
-		var gmail, li, m;
+	
+	//MY STUFF/////////////////////////
+	var currentParentId = 1;
+	var indent = 2;
+	var folderLevel = 0;
+	var bookmarkCount = 0;
+	
+	addBookmark = function(title, url){
 		li = document.createElement("li");
-		li.style.cssText = "padding-left: " + (indent * 2) + "em;";
+		li.style.cssText = "padding-left: " + (indent * folderLevel) + "em;";
+		
 		title = title.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-		if (url) {
-			li.innerHTML = "<a href=\"" + (encodeURI(url)) + "\" id='bm_" + id + "'>" + title + "</a>";
-			m = gmailRe.exec(url);
-			if (m) {
-				gmail = "https://mail.google.com/";
-				gmail += m[1] ? m[1] : "mail/";
-				gmail += "feed/atom";
-				if (url.match(/https:\/\/mail\.google\.com.*/)) {
-					updateGmailCount("bm_" + id, gmail);
-				}
-			}
-		} else {
-			// A Folder, add an arrow char and folder title
-			li.innerHTML = "&#x25be; " + title;
-		}
-		return ul.appendChild(li);
-	};
-
-	addBookmarks = function(bookmarks, indent) {
-		var b, _i, _len;
-		if (indent == null) {
-			indent = 0;
-		}
-		for (_i = 0, _len = bookmarks.length; _i < _len; _i++) {
-			b = bookmarks[_i];
-			addBookmark(b.id, b.title, b.url, indent);
-			if (!b.url) {
-				addBookmarks(b.children, indent + 1);
+		li.innerHTML = "<a href=\"" + (encodeURI(url)) + "\" id='bm_" + bookmarkCount + "'>" + title + "</a>";
+		m = gmailRe.exec(url);
+		if (m) {
+			gmail = "https://mail.google.com/";
+			gmail += m[1] ? m[1] : "mail/";
+			gmail += "feed/atom";
+			if (url.match(/https:\/\/mail\.google\.com.*/)) {
+				updateGmailCount("bm_" + bookmarkCount, gmail);
 			}
 		}
+		ul.appendChild(li);
+		bookmarkCount++;
 	};
-
-	chrome.bookmarks.getTree(function(bookmarks) {
-		return addBookmarks(bookmarks[0].children[0].children);
+	
+	addFolder = function(title){
+		li = document.createElement("li");
+		li.style.cssText = "padding-left: " + (indent * folderLevel) + "em;";
+		li.innerHTML = "&#x25be; " + title;
+		ul.appendChild(li)
+	}
+	
+	processNode = function(node) {
+		if(node.children){
+			if(node.children[0] === undefined ){
+				//Empty Folder, let's not break the system now~
+				console.log("Empty folder");
+			}
+			else { //We have a full folder!
+			
+				addFolder(node.title);
+				
+				//Track folder depth using parentId
+				folderLevel++;
+				currentParentId = node.children[0].parentId;
+				
+				//Now process any child bookmarks
+				node.children.forEach( function(child) { processNode(child); });
+			}
+		}
+		if(node.url) { //We have a bookmark!
+			
+			//If parentId changed then last folder was finished
+			if(node.parentId != currentParentId) {
+				console.log("change! " +node.parentId + currentParentId );
+				folderLevel--;
+				currentParentId = node.parentId;
+			}
+			addBookmark(node.title, node.url, node.parentId);
+		}
+	};
+	
+	chrome.bookmarks.getTree( function(bookmarks) {
+		var bookmarksBar = bookmarks[0].children[0].children;
+		bookmarksBar.forEach( function(node){
+			processNode(node);
+		});
+		console.log(bookmarksBar);
+	});
+	///////////////////////
+	
+	chrome.bookmarks.getTree( function(bookmarks) {
+		//return addBookmarks(bookmarks[0].children[0].children);
 	});
 
 }).call(this);
